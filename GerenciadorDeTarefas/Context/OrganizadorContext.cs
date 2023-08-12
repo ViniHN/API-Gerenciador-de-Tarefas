54d5b86f50ecceaaad4f5546d6a3df8cbf0c0f0d
Vinicius <vinihnunes7@gmail.com>
2023-08-12 11:54:13 -0300

    Projeto Finalizado

---
+++ GerenciadorDeTarefas/Context/OrganizadorContext.cs
@@ -0,0 +1,16 @@
+﻿using GerenciadorDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeTarefas.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+    }
+}
